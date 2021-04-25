71aa7d20d6ac50e123cb2687712f65019926449c
seregavs <sshablykin@gmail.com>
2021-04-25 21:03:40 +0300

    Add files via upload

--- rosman.py
+++ rosman.py
@@ -2,53 +2,65 @@
 import spacy
 import mysql.connector
 import os
+# import time
 from constants import *
+from progressbar import *
 
-blockid     =   0
-cursor      = None
+blockid = 0
+cursor = None
 
-sql_select_block = ( "SELECT block, filename FROM alpine.block ORDER BY block")
+sql_select_block = ("SELECT block, filename FROM alpine.block ORDER BY block")
 
 sql_insert_spacynlp = (
     "INSERT INTO `alpine`.`spacynlp` (`block`, `line`, `token_index`, `token_text`, `token_lemma`, `token_pos`, `token_tag`, `token_dep`)"
     " VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
-sql_delete_spacynlp = ( 
+sql_delete_spacynlp = (
     "DELETE FROM `alpine`.`spacynlp` WHERE (`block` = %s)")
 
-os.system('cls' if os.name == 'nt' else 'clear') # clear terminal before report run
+# clear terminal before report run
+os.system('cls' if os.name == 'nt' else 'clear')
 
-cnx = mysql.connector.connect(user=LC_DBUSER, database=LC_DATABASE, host=LC_DBHOST, password=LC_DBPASS, port = LC_DBPORT)
+cnx = mysql.connector.connect(
+    user=LC_DBUSER, database=LC_DATABASE, host=LC_DBHOST, password=LC_DBPASS, port=LC_DBPORT)
 if cnx is None:
     print('No connection made')
     quit()
 
-print(cnx)    
+print('Connected: ' + str(cnx))
 cursor = cnx.cursor()
 cursor.execute(sql_select_block)
 blocks = cursor.fetchall()
-nlp =  spacy.load('ru_core_news_lg')
+nlp = spacy.load('ru_core_news_lg')
 
 for block in blocks:
     blockid = int(block[0])
-    sql_data_delete_spacynlp = (blockid , )
+    sql_data_delete_spacynlp = (blockid, )
     cursor.execute(sql_delete_spacynlp, sql_data_delete_spacynlp)
     cnx.commit()
-    blockfile   = 'corpus/' + block[1].strip()
-    with open(blockfile, encoding="utf-8") as f:
-        lines = (line.rstrip() for line in f) 
-        lines = list(line for line in lines if line)
+    blockfile = 'corpus/' + block[1].strip()
 
+    lines = None
+    with open(blockfile, encoding="utf-8") as f:
+        lines = (line.rstrip() for line in f)
+        lines = list(line for line in lines if line)
+    linesCount = len(lines)
+    print('Block ' + str(blockid) + '('+blockfile+'): ' + str(linesCount))
+    printProgressBar(0, linesCount)
     for index, line in enumerate(lines):
         # print(index, line)
         doc = nlp(line)
         for token_index, token in enumerate(doc):
-            sql_data_insert_spacynlp = (blockid, index, token_index, token.text, token.lemma_, token.pos_, token.tag_, token.dep_)
+            sql_data_insert_spacynlp = (
+                blockid, index, token_index, token.text, token.lemma_, token.pos_, token.tag_, token.dep_)
             # print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_)
             if cursor:
                 cursor.execute(sql_insert_spacynlp, sql_data_insert_spacynlp)
-                # print(cursor.lastrowid)
-                cnx.commit()
-        print('block ', blockid, ' line ', index, ' handled')
-    print('block ', blockid, ' done')
-cnx.close()   
+        # print('block ', blockid, ' line ', index, ' handled')
+        if index%LC_COMMIT_EVERY_N_ROWS == 0:
+            cnx.commit()
+            printProgressBar(index + 1, linesCount)
+    printProgressBar( linesCount, linesCount)
+    cnx.commit()
+    # print('block ', blockid, ' done')
+cnx.close()
 print('All done')
